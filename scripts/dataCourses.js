b9c9860e34bddd0b2c6c4d9dd38079477960ffe1
mvgarcia <mv.garcia@uniandes.edu.co>
2021-04-08 18:49:02 -0500

    Taller Typescript 1

---
+++ scripts/dataCourses.js
@@ -0,0 +1,12 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de software en equipo", "Ruby Casallas", 3),
+    new Course("Lab Métodos computacionales", "Diego Useche", 1),
+    new Course("Métodos computacionales", "Alejandro Segura", 3),
+    new Course("Francés 4", "Claudia Peláez", 3),
+    new Course("Diseño y análisis de Algoritmos", "Jorge Duitama", 3),
+    new Course("Arquitectura empresarial", "María Camila Romero", 3),
+    new Course("Física estadística", "Juan Manuel Pedraza", 3),
+    new Course("Coloquio 2 de Física", "Beatriz Sabogal", 1),
+    new Course("Mecánica Cuántica I", "Andrés Florez", 3)
+];
